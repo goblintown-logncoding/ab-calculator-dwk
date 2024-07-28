3296900c3c142a5a282d96fed7ae39d8c557d4f0
Daewoong <kodw.kor@gmail.com>
2024-07-28 16:49:37 +0900

    feat: create Content, Footer, Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import React from "react";
+import Header from './components/Header';
+import Content from './components/Content';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-      hello
+      <Header />
+      <Content />
+      <Footer />
     </div>
   )
 }
